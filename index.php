73787ef1568a2139a4aaab128ebc9e89c46766b8
Abdul Rafique <princeabdulrafique@gmail.com>
2021-03-09 15:53:30 +0500

    I have made simple todo app using PHP

---
+++ index.php
@@ -0,0 +1,120 @@
+<?php
+    // Database connection
+    $db_host = "localhost";
+    $db_username = "root";
+    $db_password = "";
+    $db_name = "todo-app";
+
+    $db_conn = mysqli_connect($db_host, $db_username, $db_password, $db_name) or die();
+
+    if(mysqli_connect_error()){
+        echo "<script>console.log('Database connection failed.')</script>";
+    }
+
+    echo "<script>console.log('Databse connected successfully.')</script>";
+
+    // Adding todos to DB
+    if(isset($_POST["submit"])){
+        $todo = $_POST["todo"];
+        $insQuery = "INSERT INTO tasks (body) VALUES ('$todo')";
+
+        if(!empty($todo)){
+            $insert = mysqli_query($db_conn, $insQuery);
+            unset($todo);
+            
+            $reloadPage = $_SERVER["PHP_SELF"];
+            
+            header("Location: $reloadPage");
+        }
+        else{
+            echo "<script>alert('Please write a task todo.')</script>";
+        }
+    }
+
+    // Deleting todo from database
+    if(isset($_POST["delete"])){
+        $todoId = $_POST["todo-id"];
+
+        $delQuery = "DELETE FROM tasks WHERE id = $todoId";
+
+        if(!empty($todoId)){
+            $delete = mysqli_query($db_conn, $delQuery);
+            unset($todoId);
+
+            $reloadPage = $_SERVER["PHP_SELF"];
+
+            header("Location: $reloadPage");
+        }
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Todo App</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Todo App</h1>
+        <div class="form-wrap">
+            <form method="POST" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
+                <div class="input-group">
+                    <input type="text" name="todo" id="todo-input" class="form-control">
+                    <button type="submit" name="submit" class="btn-add">Add Todo</button>
+                </div>
+            </form>
+        </div>
+        <?php
+            $selQuery = "SELECT * FROM tasks";
+            $select = mysqli_query($db_conn, $selQuery);
+            
+            if(mysqli_num_rows($select) > 0){
+                ?>
+                <div class="table-wrap">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Todos</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                                    // echo "<script>alert('Data found')</script>";
+                                    $a = 0;
+                                    while($row = mysqli_fetch_assoc($select)){
+                            ?>
+                                    <tr>
+                                        <td><?php echo $a += 1; ?></td>
+                                        <td><?php echo $row["body"]; ?></td>
+                                        <td>
+                                            <?php ?>
+                                                <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post">
+                                                    <input type="text" name="todo-id" id="todo-id" value="<?php echo $row["id"] ?>">
+                                                    <button type="submit" name="delete" class='btn-del'>Delete</button>
+                                                </form>
+                                            <?php ?>
+                                        </td>
+                                    </tr>
+                            <?php } ?>
+                        </tbody>
+                    </table>
+                </div>
+        <?php
+            }
+
+            else{
+                echo "<div class='no-data'>
+                    <img src='no-data.svg' alt='No Data Found' width='300' />
+                    <h3>No Data Found</h3>
+                </div>";
+            }
+        ?>
+        <p class="copyright">Made with &#128151; by Abdul Rafique</p>
+    </div>
+</body>
+</html>
